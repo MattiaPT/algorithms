9dfa22f7e7c890a7b8f401dafcc5fd6d6ec83ea7
Mattia Pepin Taiana <mattia.taiana@gmail.com>
2022-12-13 21:42:16 +0100

    added tests, however not working yet

--- search-algos/test/SearchTest.java
+++ search-algos/test/SearchTest.java
@@ -80,6 +80,33 @@ public class SearchTest {
 		return n;
 	}
 	public Graph createGraph() {
-		return null;
+		ArrayList<ArrayList<Node<Integer>>> adj = new ArrayList<ArrayList<Node<Integer>>>();
+		ArrayList<Node<Integer>> node_1_adj = new ArrayList<Node<Integer>>();
+		ArrayList<Node<Integer>> node_2_adj = new ArrayList<Node<Integer>>();
+		ArrayList<Node<Integer>> node_3_adj = new ArrayList<Node<Integer>>();
+		ArrayList<Node<Integer>> node_4_adj = new ArrayList<Node<Integer>>();
+		ArrayList<Node<Integer>> node_5_adj = new ArrayList<Node<Integer>>();
+		Node<Integer> node_1 = new Node(0);
+		Node<Integer> node_2 = new Node(1);
+		Node<Integer> node_3 = new Node(2);
+		Node<Integer> node_4 = new Node(3);
+		Node<Integer> node_5 = new Node(4);
+		node_1_adj.add(node_2);
+		node_1_adj.add(node_3);
+		node_1_adj.add(node_5);
+		node_2_adj.add(node_5);
+		node_3_adj.add(node_4);
+		node_4_adj.add(node_5);
+		node_5_adj.add(node_3);
+		
+		Integer[][] weights = new Integer[adj.size()][adj.size()];
+		weights[0] = new Integer[] {0, 4, 2, 0, 18};
+		weights[1] = new Integer[] {0, 0, 0, 0, 1};
+		weights[2] = new Integer[] {0, 0, 0, 1, 0};
+		weights[3] = new Integer[] {0, 0, 0, 0, 1};
+		weights[4] = new Integer[] {0, 0, 1, 0, 0};
+		
+		
+		return new Graph(adj, weights);
 	}
 }
