c8b0b7e886a7f5db7aa2478c02fbacf837a1565b
Mattia Pepin Taiana <mattia.taiana@gmail.com>
2023-01-15 15:20:29 +0100

    changed structure of testing equality of arrays

--- 3_dp-java/test/SubsetSumTest.java
+++ 3_dp-java/test/SubsetSumTest.java
@@ -1,7 +1,6 @@
-import static org.junit.Assert.assertArrayEquals;
+import org.junit.Assert;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import java.util.Arrays;
 
 /*
  * Author: Mattia
@@ -18,11 +17,7 @@ public class SubsetSumTest {
 		boolean[][] dp_table = test1.calcSubsetSum();
 		
 		assertEquals(true, test1.evenSplitIsPossible(dp_table));
-		int[][] solutions = test1.readSolution(dp_table);
-		int[][] ideal = new int[][] {new int[] {15, 3, 12, 0, 0, 0, 0, 0, 0, 0}, new int[] {7, 8, 1, 2, 9, 3, 0, 0, 0, 0}};
-		for (int i = 0; i < solutions[0].length; i++) {
-			assertEquals(solutions[0][i], ideal[0][i]);
-			assertEquals(solutions[1][i], ideal[1][i]);
-		}
+		int[][] solution = new int[][] {new int[] {15, 3, 12, 0, 0, 0, 0, 0, 0, 0}, new int[] {7, 8, 1, 2, 9, 3, 0, 0, 0, 0}};
+		Assert.assertArrayEquals(solution, test1.readSolution(dp_table));
 	}
 }
