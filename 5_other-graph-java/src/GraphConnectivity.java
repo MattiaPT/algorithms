0266d896ae1689009b88ee135c949f679acfb2fa
MattiaPT <mattia.taiana@gmail.com>
2023-03-13 21:20:04 +0100

    various changes

--- 5_other-graph-java/src/GraphConnectivity.java
+++ 5_other-graph-java/src/GraphConnectivity.java
@@ -1,7 +1,24 @@
 /*
  * Author: Mattia
  * Date: 12.03.2023
+ *
+ * Implementing Algorithms learnt to
  */
+
+
 public class GraphConnectivity {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+// FIXME: not yet added GraphTheory to project
+class Graph {
+
+}
+class Node {
+
+}
+class Edge {
 
 }
